5b77ebd70fa790971f209186bb7072d6dadce21e
Stefan Mandel <mandelst@gmail.com>
2019-10-10 10:15:09 +0200

    to junit 5

--- src/test/java/de/andrena/refactoringlikecompilers/ssa/UglyClassTest.java
+++ src/test/java/de/andrena/refactoringlikecompilers/ssa/UglyClassTest.java
@@ -1,134 +1,134 @@
 package de.andrena.refactoringlikecompilers.ssa;
 
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.hamcrest.Description;
 import org.hamcrest.TypeSafeDiagnosingMatcher;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import de.andrena.refactoringlikecompilers.ssa.UglyClass;
 
-public class UglyClassTest {
+class UglyClassTest {
 	private UglyClass frankMustermann = new UglyClass("m", "Frank", "Mustermann");
 	private UglyClass erikaMusterfrau = new UglyClass("f", "Erika", "Musterfrau");
 
 	@Test
-	public void testGivenCorrectLastNameItMatches() {
+	void testGivenCorrectLastNameItMatches() {
 		assertThat(frankMustermann, matchesName("Mustermann"));
 	}
 
 	@Test
-	public void testGivenEmptyStringDoesNotMatch() {
+	void testGivenEmptyStringDoesNotMatch() {
 		assertThat(frankMustermann, doesNotMatchName(""));
 	}
 
 	@Test
-	public void testGivenOnlyWrongLastNameItDoesNotMatch() {
+	void testGivenOnlyWrongLastNameItDoesNotMatch() {
 		assertThat(frankMustermann, doesNotMatchName("Anders"));
 	}
 
 	@Test
-	public void testGivenCorrectFirstAndLastNameItMatches() {
+	void testGivenCorrectFirstAndLastNameItMatches() {
 		assertThat(frankMustermann, matchesName("Frank Mustermann"));
 	}
 
 	@Test
-	public void testGivenIncorrectFirstAndCorrectLastNameItDoesNotMatch() {
+	void testGivenIncorrectFirstAndCorrectLastNameItDoesNotMatch() {
 		assertThat(frankMustermann, doesNotMatchName("Bernd Mustermann"));
 	}
 
 	@Test
-	public void testGivenCorrectFirstAndIncorrectLastNameItDoesNotMatch() {
+	void testGivenCorrectFirstAndIncorrectLastNameItDoesNotMatch() {
 		assertThat(frankMustermann, doesNotMatchName("Frank Anders"));
 	}
 
 	@Test
-	public void testGivenCorrectFirstAndLastNameButWrongGenderItDoesNotMatch() {
+	void testGivenCorrectFirstAndLastNameButWrongGenderItDoesNotMatch() {
 		assertThat(frankMustermann, doesNotMatchName("Fr. Frank Mustermann"));
 	}
 
 	@Test
-	public void testGivenOnlyCorrectLastNameButWrongGenderItMatches() {
+	void testGivenOnlyCorrectLastNameButWrongGenderItMatches() {
 		assertThat(frankMustermann, matchesName("Fr. Mustermann"));
 	}
 
 	@Test
-	public void testGivenCorrectFirstAndLastNameAndCorrectGenderItMatches() {
+	void testGivenCorrectFirstAndLastNameAndCorrectGenderItMatches() {
 		assertThat(frankMustermann, matchesName("Hr. Frank Mustermann"));
 	}
 
 	@Test
-	public void testGivenIncorrectFirstNameButCorrectLastNameAndCorrectGenderItDoesNotMatch() {
+	void testGivenIncorrectFirstNameButCorrectLastNameAndCorrectGenderItDoesNotMatch() {
 		assertThat(frankMustermann, doesNotMatchName("Hr. Bernd Mustermann"));
 	}
 
 	@Test
-	public void testGivenIncorrectLastNameButCorrectFirstNameAndCorrectGenderItDoesNotMatch() {
+	void testGivenIncorrectLastNameButCorrectFirstNameAndCorrectGenderItDoesNotMatch() {
 		assertThat(frankMustermann, doesNotMatchName("Hr. Frank Anders"));
 	}
 
 	@Test
-	public void testGivenCorrectLastNameButUnknownGenderDoesNotMatch() {
+	void testGivenCorrectLastNameButUnknownGenderDoesNotMatch() {
 		assertThat(frankMustermann, doesNotMatchName("Mr. Mustermann"));
 	}
 
 	@Test
-	public void testGivenCorrectLastNameAndGenderItMatches() {
+	void testGivenCorrectLastNameAndGenderItMatches() {
 		assertThat(frankMustermann, matchesName("Hr. Mustermann"));
 	}
 
 	@Test
-	public void testGivenIncorrectLastNameButCorrectGenderDoesNotMatch() {
+	void testGivenIncorrectLastNameButCorrectGenderDoesNotMatch() {
 		assertThat(frankMustermann, doesNotMatchName("Hr. Anders"));
 	}
 
 	@Test
-	public void testFemaleGivenCorrectLastNameAndGenderItMatches() {
+	void testFemaleGivenCorrectLastNameAndGenderItMatches() {
 		assertThat(erikaMusterfrau, matchesName("Fr. Musterfrau"));
 	}
 
 	@Test
-	public void testFemaleGivenIncorrectLastNameButCorrectGenderItDoesNotMatch() {
+	void testFemaleGivenIncorrectLastNameButCorrectGenderItDoesNotMatch() {
 		assertThat(erikaMusterfrau, doesNotMatchName("Fr. Anders"));
 	}
 
 	@Test
-	public void testFemaleGivenCorrectFirstAndLastNameAndGenderItMatches() {
+	void testFemaleGivenCorrectFirstAndLastNameAndGenderItMatches() {
 		assertThat(erikaMusterfrau, matchesName("Fr. Erika Musterfrau"));
 	}
 
 	@Test
-	public void testFemaleGivenCorrectLastNameAndWrongGenderItDoesNotMatch() {
+	void testFemaleGivenCorrectLastNameAndWrongGenderItDoesNotMatch() {
 		assertThat(erikaMusterfrau, doesNotMatchName("Hr. Musterfrau"));
 	}
 
 	@Test
-	public void testFemaleGivenCorrectFirstAndLastNameItMatches() {
+	void testFemaleGivenCorrectFirstAndLastNameItMatches() {
 		assertThat(erikaMusterfrau, matchesName("Erika Musterfrau"));
 	}
 
 	@Test
-	public void testFemaleGivenCorrectLastNameButUnknownGenderDoesNotMatch() {
+	void testFemaleGivenCorrectLastNameButUnknownGenderDoesNotMatch() {
 		assertThat(erikaMusterfrau, doesNotMatchName("Ms. Musterfrau"));
 	}
 
 	@Test
-	public void testFemaleGivenIncorrectFirstNameButCorrectLastNameAndCorrectGenderItDoesNotMatch() {
+	void testFemaleGivenIncorrectFirstNameButCorrectLastNameAndCorrectGenderItDoesNotMatch() {
 		assertThat(erikaMusterfrau, doesNotMatchName("Fr. Beate Musterfrau"));
 	}
 
 	@Test
-	public void testFemaleGivenIncorrectLastNameButCorrectFirstNameAndCorrectGenderItDoesNotMatch() {
+	void testFemaleGivenIncorrectLastNameButCorrectFirstNameAndCorrectGenderItDoesNotMatch() {
 		assertThat(erikaMusterfrau, doesNotMatchName("Hr. Erika Anders"));
 	}
 
 	@Test
-	public void testFemaleGivenCorrectShortenedFirstNameAndCorrectLastNameItMatches() {
+	void testFemaleGivenCorrectShortenedFirstNameAndCorrectLastNameItMatches() {
 		assertThat(erikaMusterfrau, matchesName("E. Musterfrau"));
 	}
 
 	@Test
-	public void testFemaleGivenCorrectShortenedFirstNameButIncorrectLastNameItDoesNotMatch() {
+	void testFemaleGivenCorrectShortenedFirstNameButIncorrectLastNameItDoesNotMatch() {
 		assertThat(erikaMusterfrau, doesNotMatchName("E. Anders"));
 	}
 
